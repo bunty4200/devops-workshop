87dded2e97d7eed09a124a4538cade2c83693731
bunty4200 <chauhanjayraj8648@gmail.com>
2024-04-08 12:37:02 +0530

    added v4 version of vpc

--- terraform-code/V4-EC2_with_VPC_for_each.tf
+++ terraform-code/V4-EC2_with_VPC_for_each.tf
@@ -10,6 +10,9 @@ resource "aws_instance" "demo-server" {
     vpc_security_group_ids = [ aws_security_group.demo-sg.id ]
     subnet_id = aws_subnet.dpp-public-subnet-01.id
 for_each = toset(["Jenkins-master", "build-slave", "ansible"])
+    tags = {
+      Name = "${each.key}"
+    }
 }
 
 

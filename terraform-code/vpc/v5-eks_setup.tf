7424f2c565503e77b52e02676731dee2e1541d72
bunty4200 <chauhanjayraj8648@gmail.com>
2024-04-15 13:33:17 +0530

    added kubernetes code in devops folder

--- terraform-code/vpc/v5-eks_setup.tf
+++ terraform-code/vpc/v5-eks_setup.tf
@@ -110,15 +110,15 @@ resource "aws_route_table_association" "dpp-rta-public-subnet-02" {
 }
 
  module "sgs" {
-    source = "../sg_eks"
+   source = "../sg_eks"
     vpc_id     =     aws_vpc.dpp-vpc.id
     subnet_ids = [aws_subnet.dpp-public-subnet-01.id,aws_subnet.dpp-public-subnet-02.id]
     sg_ids = module.sgs.security_group_public
   }
 
   module "eks" {
-      source = "../eks"
-      vpc_id     =     aws_vpc.dpp-vpc.id
-      subnet_ids = [aws_subnet.dpp-public-subnet-01.id,aws_subnet.dpp-public-subnet-02.id]
-      sg_ids = module.sgs.security_group_public
-  }
+   source = "../eks"
+   vpc_id     =     aws_vpc.dpp-vpc.id
+   subnet_ids = [aws_subnet.dpp-public-subnet-01.id,aws_subnet.dpp-public-subnet-02.id]
+   sg_ids = module.sgs.security_group_public
+   }
